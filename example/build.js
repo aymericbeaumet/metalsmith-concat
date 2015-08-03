66a68ff0df092c575924d8ef709284793bac7241
Aymeric Beaumet <aymeric@beaumet.me>
2015-08-02 20:35:53 -0400

    refactor: close some issues
    - Switch to standard JavaScript (https://github.com/feross/standard)
    - Switch to tape (https://github.com/substack/tape)
    - Allow to use minimatch patterns as array (closes https://github.com/aymericbeaumet/metalsmith-concat/issues/6 https://github.com/aymericbeaumet/metalsmith-concat/issues/9)
    - Normalize file paths: add Windows support (closes https://github.com/aymericbeaumet/metalsmith-concat/issues/8)
    - It is possible to pass a custom `options.insertNewline` string
    - Add Node 0.12 to Travis
    - 100% code coverage

--- example/build.js
+++ example/build.js
@@ -1,9 +1,9 @@
-'use strict';
+#!/usr/bin/env node
+'use strict'
 
-var Metalsmith = require('metalsmith');
-var concat = require('../lib'); // require('metalsmith-concat');
+var Metalsmith = require('metalsmith')
+var concat = require('..') // require('metalsmith-concat')
 
 Metalsmith(__dirname)
   .use(concat({ files: '**/*.css', output: 'main.css' }))
-  .build(function(err) { if (err) { throw err; } })
-;
+  .build(function (err) { if (err) { throw err } })
