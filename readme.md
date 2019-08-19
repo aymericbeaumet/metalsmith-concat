975e875b48aa215505b4c0a6a555c65292756bf9
Aymeric Beaumet <hi@aymericbeaumet.com>
2019-08-20 01:10:05 +0200

    maintenance

--- readme.md
+++ readme.md
@@ -1,18 +1,22 @@
-# metalsmith-concat [![NPM version](https://img.shields.io/npm/v/metalsmith-concat.svg?style=flat-square)](https://www.npmjs.com/package/metalsmith-concat) [![Build status](https://img.shields.io/travis/aymericbeaumet/metalsmith-concat/master.svg?style=flat-square)](https://travis-ci.org/aymericbeaumet/metalsmith-concat) [![Dependencies](https://img.shields.io/david/aymericbeaumet/metalsmith-concat.svg?style=flat-square)](https://david-dm.org/aymericbeaumet/metalsmith-concat)
+# metalsmith-concat [![Build Status](https://travis-ci.org/aymericbeaumet/metalsmith-concat.svg?branch=master)](https://travis-ci.org/aymericbeaumet/metalsmith-concat)
 
-A Metalsmith plugin to concatenate files.
+> A Metalsmith plugin to concatenate files
 
-## Installation
+## Install
 
-```javascript
-$ npm install metalsmith-concat
+```shell
+npm install --save metalsmith-concat
 ```
 
 ## Usage
 
+This plugin enables you to concatenate files together.
+
 ### CLI
 
-```javascript
+**metalsmith.json**
+
+```json
 {
   "plugins": {
     "metalsmith-concat": {
@@ -23,83 +27,92 @@ $ npm install metalsmith-concat
 }
 ```
 
-### JavaScript
+### API
 
 ```javascript
-import MetalSmith from "metalsmith";
-import concat from "metalsmith-concat";
+import metalsmith from 'metalsmith'
+import metalsmithConcat from 'metalsmith-concat'
 
-new Metalsmith(__dirname).use(
-  concat({
-    files: "styles/**/*.css",
-    output: "styles/app.css"
+metalsmith(__dirname).use(
+  metalsmithConcat({
+    files: 'styles/**/*.css',
+    output: 'styles/app.css',
   })
-);
+)
 ```
 
-#### files
+**metalsmithConcat(options)**
 
-Type: `String` / `String[]`
+#### options
+
+Type: `Object`
+Default: `{}`
+
+#### options.files
+
+Type: `string` / `string[]`
 Default: `'**/*'`
 
 This defines which files are concatenated. This string will be interpreted as a
 [minimatch](https://github.com/isaacs/minimatch) pattern. An array of strings
-will be interpreted as minimatch patterns, in this case the order of the
-patterns matters (it will determine the order in which the files are
+will be interpreted as distinct minimatch patterns, in this case the order of
+the patterns matters (it will determine the order in which the files are
 concatenated).
 
-_Note: this is relative to the source path and the [search paths](https://github.com/aymericbeaumet/metalsmith-concat#searchpaths) (if any)._
+_Note: this is relative to both the source path and the [search
+paths](https://github.com/aymericbeaumet/metalsmith-concat#optionssearchpaths)
+(if any)._
 
-#### output
+#### options.output
 
-Type: `String`
+Type: `string`
 
 It represents the filepath where the concatenated content will be outputted.
 This option is **mandatory**.
 
 _Note: this is relative to the destination path._
 
-#### forceOutput
+#### options.forceOutput
 
-Type: `Boolean`
+Type: `boolean`
 Default: `false`
 
 By default metalsmith-concat return an error if the output file already exists.
 You can force an existing output file to be overwritten by setting this option
 to `true`.
 
-#### insertNewLine
+#### options.insertNewLine
 
-Type: `Boolean` | `String`
+Type: `boolean` | `string`
 Default: `true`
 
 Whether a trailing new line (`\n`) should be appended after each concatenated
-file. Unless you face a problem, you should keep this option enabled as removing
-it could cause invalid concatenated files (see [this
+file. Unless you face a problem, you should keep this option enabled as
+removing it could cause invalid concatenated files (see [this
 article](http://evanhahn.com/newline-necessary-at-the-end-of-javascript-files/)).
-It is also possible to pass a string, in which case it will be used instead of
-`\n`.
+It is also possible to pass a string, in which case it will be used instead
+of `\n`.
 
-#### keepConcatenated
+#### options.keepConcatenated
 
-Type: `Boolean`
+Type: `boolean`
 Default: `false`
 
 Whether to keep the files which were concatenated. By default they are not kept
 and deleted from the build (thus only keeping the newly created file at
 `options.output`).
 
-#### searchPaths
+#### options.searchPaths
 
-Type: `String` / `String[]`
+Type: `string` / `string[]`
 Default: `[]`
 
-The additional paths to search after the `src` directory given to Metalsmith.
-The paths are resolved relatively to the metalsmith root directory. Absolute
-paths can also been given. An ignore pattern is applied on the results to make
-sure the `src` directory is not matched twice from a search path.
+Specify additional paths to search. The paths are resolved relatively to
+Metalsmith's root directory. Absolute paths are also supported. An ignore
+pattern is applied on the results to make sure the `src` directory is not
+matched twice from a custom search path.
 
-Let's consider the following example:
+Let's consider the example below:
 
 ```
 src/
@@ -113,7 +126,7 @@ node_modules/
 It is possible to include _React_ from the _node_modules_ before your _index.js_
 from _src_ by using the following configuration:
 
-```js
+```javascript
 {
   files: [
     'react/dist/react.min.js', // found in node_modules
@@ -122,93 +135,3 @@ from _src_ by using the following configuration:
   searchPaths: ['node_modules'],
 }
 ```
-
-## Changelog
-
-- 6.0.3
-
-  - Update dependencies
-
-- 6.0.0
-
-  - Expect and produce a `Buffer` for the `contents` key
-
-- 5.0.2
-
-  - Simplify the path given in the pattern
-
-- 5.0.1
-
-  - Fix 100% code coverage on Node 4 (sandboxed-module -> proxyquire)
-
-- 5.0.0
-
-  - Add the `searchPaths` option
-  - Switch test suite to nyc + ava
-
-- 4.2.2
-
-  - Lightweight npm-shrinkwrap
-
-- 4.2.1
-
-  - Update license to public domain
-
-- 4.2.0
-
-  - Require options.output to be a non-empty string
-
-- 4.1.1
-
-  - Exclude test files from CodeClimate
-
-- 4.1.0
-
-  - It is possible to override existing output file check by setting `options.forceOutput` to true ([#19](https://github.com/aymericbeaumet/metalsmith-concat/issues/19))
-  - Handle metalsmith errors gracefully as described [here](http://www.robinthrift.com/posts/metalsmith-part-3-refining-our-tools/)
-
-- 4.0.1
-
-  - Drop node 0.10 and 0.12 support as Metalsmith requires generators
-
-- 4.0.0
-
-  - Update dependencies
-  - Remove `npm-shrinkwrap`
-  - Fix linting
-  - Add node 4 and 5 to Travis/AppVeyor
-
-- 3.0.0
-
-  - An array of minimatch patterns can now be passed as `options.files` ([#6](https://github.com/aymericbeaumet/metalsmith-concat/issues/6), [#9](https://github.com/aymericbeaumet/metalsmith-concat/issues/9))
-  - File paths are normalized, hence making this plugin working on Windows
-    ([#8](https://github.com/aymericbeaumet/metalsmith-concat/issues/8))
-  - It is possible to pass a custom `options.insertNewline` character
-
-- 2.0.0
-
-  - Add a newline at the end of each concatenated file by default
-    ([#4](https://github.com/aymericbeaumet/metalsmith-concat/pull/4))
-
-- 1.0.1
-
-  - Add the possibility to pass an array of files instead of a matching pattern
-    ([#2](https://github.com/aymericbeaumet/metalsmith-concat/pull/2))
-
-- 1.0.0
-
-  - Bump stable
-
-- 0.0.2
-
-  - Fix an issue with file content
-
-- 0.0.1
-  - Working plugin
-
-## License
-
-[![CC0](http://i.creativecommons.org/p/zero/1.0/88x31.png)](http://creativecommons.org/publicdomain/zero/1.0/)
-
-To the extent possible under law, [Aymeric Beaumet](https://aymericbeaumet.com)
-has waived all copyright and related or neighboring rights to this work.
---
+++ test.js
@@ -0,0 +1,579 @@
+const path = require('path')
+const test = require('ava')
+const sinon = require('sinon')
+const concat = require('.')
+
+function filesFixture() {
+  return {
+    'first/file': { contents: Buffer.from('lorem') },
+    'second/file': { contents: Buffer.from(' ') },
+    'third/file': { contents: Buffer.from('ipsum') },
+  }
+}
+
+function metalsmithFixture() {
+  return {
+    _directory: path.resolve(__dirname, 'fixture'),
+  }
+}
+
+test.afterEach.always(() => sinon.restore())
+
+test.cb('metalsmith-concat should concatenate all files by default', t => {
+  t.plan(2)
+  const files = filesFixture()
+  const plugin = concat({ output: 'output/path' })
+  plugin(files, metalsmithFixture(), error => {
+    t.falsy(error)
+    t.deepEqual(files, {
+      'output/path': { contents: Buffer.from('lorem\n \nipsum\n') },
+    })
+    t.end()
+  })
+})
+
+test.cb(
+  'metalsmith-concat should only concatenate the specified files if options.files is passed as a string',
+  t => {
+    t.plan(2)
+    const files = filesFixture()
+    const plugin = concat({ files: 'first/*', output: 'output/path' })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'output/path': { contents: Buffer.from('lorem\n') },
+        'second/file': { contents: Buffer.from(' ') },
+        'third/file': { contents: Buffer.from('ipsum') },
+      })
+      t.end()
+    })
+  }
+)
+
+test.cb(
+  'metalsmith-concat should only concatenate the specified files if options.files is passed as a strings array',
+  t => {
+    t.plan(2)
+    const files = filesFixture()
+    const plugin = concat({
+      files: ['first/*', 'third/*'],
+      output: 'output/path',
+    })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'output/path': { contents: Buffer.from('lorem\nipsum\n') },
+        'second/file': { contents: Buffer.from(' ') },
+      })
+      t.end()
+    })
+  }
+)
+
+test.cb(
+  'metalsmith-concat should concatenate no files if the matching pattern is an empty string',
+  t => {
+    t.plan(2)
+    const files = filesFixture()
+    const plugin = concat({ files: '', output: 'output/path' })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'output/path': { contents: Buffer.from('') },
+        'first/file': { contents: Buffer.from('lorem') },
+        'second/file': { contents: Buffer.from(' ') },
+        'third/file': { contents: Buffer.from('ipsum') },
+      })
+      t.end()
+    })
+  }
+)
+
+test.cb(
+  'metalsmith-concat should concatenate no files if the matching pattern is an empty array',
+  t => {
+    t.plan(2)
+    const files = filesFixture()
+    const plugin = concat({ files: [], output: 'output/path' })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'output/path': { contents: Buffer.from('') },
+        'first/file': { contents: Buffer.from('lorem') },
+        'second/file': { contents: Buffer.from(' ') },
+        'third/file': { contents: Buffer.from('ipsum') },
+      })
+      t.end()
+    })
+  }
+)
+
+test.cb(
+  'metalsmith-concat should concatenate file in the order of the files keys',
+  t => {
+    t.plan(2)
+    const files = filesFixture()
+    const plugin = concat({ output: 'output/path' })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'output/path': { contents: Buffer.from('lorem\n \nipsum\n') },
+      })
+      t.end()
+    })
+  }
+)
+
+test.cb(
+  'metalsmith-concat should concatenate file in the order of the files keys / options.files patterns',
+  t => {
+    t.plan(2)
+    const files = filesFixture()
+    const plugin = concat({
+      files: ['third/*', 'second/*', 'first/*'],
+      output: 'output/path',
+    })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'output/path': { contents: Buffer.from('ipsum\n \nlorem\n') },
+      })
+      t.end()
+    })
+  }
+)
+
+test.cb(
+  'metalsmith-concat should insert the new line as `\\n` by default',
+  t => {
+    t.plan(2)
+    const files = filesFixture()
+    const plugin = concat({ files: 'first/*', output: 'output/path' })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'output/path': { contents: Buffer.from('lorem\n') },
+        'second/file': { contents: Buffer.from(' ') },
+        'third/file': { contents: Buffer.from('ipsum') },
+      })
+      t.end()
+    })
+  }
+)
+
+test.cb(
+  'metalsmith-concat should insert the new line as `\\n` if options.insertNewline is true',
+  t => {
+    t.plan(2)
+    const files = filesFixture()
+    const plugin = concat({
+      files: 'first/*',
+      output: 'output/path',
+      insertNewline: true,
+    })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'output/path': { contents: Buffer.from('lorem\n') },
+        'second/file': { contents: Buffer.from(' ') },
+        'third/file': { contents: Buffer.from('ipsum') },
+      })
+      t.end()
+    })
+  }
+)
+
+test.cb(
+  'metalsmith-concat should not insert the new line if options.insertNewline is false',
+  t => {
+    t.plan(2)
+    const files = filesFixture()
+    const plugin = concat({
+      files: 'first/*',
+      output: 'output/path',
+      insertNewline: false,
+    })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'output/path': { contents: Buffer.from('lorem') },
+        'second/file': { contents: Buffer.from(' ') },
+        'third/file': { contents: Buffer.from('ipsum') },
+      })
+      t.end()
+    })
+  }
+)
+
+test.cb(
+  'metalsmith-concat should support custom new line if options.insertNewline is a string',
+  t => {
+    t.plan(2)
+    const files = filesFixture()
+    const plugin = concat({
+      files: 'first/*',
+      output: 'output/path',
+      insertNewline: '\r\n',
+    })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'output/path': { contents: Buffer.from('lorem\r\n') },
+        'second/file': { contents: Buffer.from(' ') },
+        'third/file': { contents: Buffer.from('ipsum') },
+      })
+      t.end()
+    })
+  }
+)
+
+test.cb('metalsmith-concat should delete the source files by default', t => {
+  t.plan(2)
+  const files = filesFixture()
+  const plugin = concat({ output: 'output/path' })
+  plugin(files, metalsmithFixture(), error => {
+    t.falsy(error)
+    t.deepEqual(files, {
+      'output/path': { contents: Buffer.from('lorem\n \nipsum\n') },
+    })
+    t.end()
+  })
+})
+
+test.cb(
+  'metalsmith-concat should delete the concatenated original files if options.keepConcatenated is false',
+  t => {
+    t.plan(2)
+    const files = filesFixture()
+    const plugin = concat({ output: 'output/path', keepConcatenated: false })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'output/path': { contents: Buffer.from('lorem\n \nipsum\n') },
+      })
+      t.end()
+    })
+  }
+)
+
+test.cb(
+  'metalsmith-concat should keep the concatenated original files if options.keepConcatenated is true',
+  t => {
+    t.plan(2)
+    const files = filesFixture()
+    const plugin = concat({ output: 'output/path', keepConcatenated: true })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'output/path': { contents: Buffer.from('lorem\n \nipsum\n') },
+        'first/file': { contents: Buffer.from('lorem') },
+        'second/file': { contents: Buffer.from(' ') },
+        'third/file': { contents: Buffer.from('ipsum') },
+      })
+      t.end()
+    })
+  }
+)
+
+test.cb('metalsmith-concat should throw if no options object is passed', t => {
+  t.plan(2)
+  try {
+    concat()
+  } catch (error) {
+    t.true(error instanceof Error)
+    t.is(
+      error.message,
+      '`options.output` is mandatory and has to be a non-empty string'
+    )
+    t.end()
+  }
+})
+
+test.cb(
+  'metalsmith-concat should throw if options.output is not defined',
+  t => {
+    t.plan(2)
+    try {
+      concat({})
+    } catch (error) {
+      t.true(error instanceof Error)
+      t.is(
+        error.message,
+        '`options.output` is mandatory and has to be a non-empty string'
+      )
+      t.end()
+    }
+  }
+)
+
+test.cb(
+  'metalsmith-concat should throw error if options.output is not a string',
+  t => {
+    t.plan(2)
+    try {
+      concat({ output: false })
+    } catch (error) {
+      t.true(error instanceof Error)
+      t.is(
+        error.message,
+        '`options.output` is mandatory and has to be a non-empty string'
+      )
+      t.end()
+    }
+  }
+)
+
+test.cb(
+  'metalsmith-concat should throw error if options.output is an empty string',
+  t => {
+    t.plan(2)
+    try {
+      concat({ output: '' })
+    } catch (error) {
+      t.true(error instanceof Error)
+      t.is(
+        error.message,
+        '`options.output` is mandatory and has to be a non-empty string'
+      )
+      t.end()
+    }
+  }
+)
+
+test.cb(
+  'metalsmith-concat should return an error if options.output already exists by default',
+  t => {
+    t.plan(2)
+    const plugin = concat({ output: 'output/path' })
+    plugin({ 'output/path': {} }, metalsmithFixture(), error => {
+      t.true(error instanceof Error)
+      t.is(
+        error.message,
+        'The file "output/path" already exists, see the documentation for \'options.forceOutput\''
+      )
+      t.end()
+    })
+  }
+)
+
+test.cb(
+  'metalsmith-concat should return an error if options.output already exists when indicated as option',
+  t => {
+    t.plan(2)
+    const plugin = concat({ output: 'output/path', forceOutput: false })
+    plugin({ 'output/path': {} }, metalsmithFixture(), error => {
+      t.true(error instanceof Error)
+      t.is(
+        error.message,
+        'The file "output/path" already exists, see the documentation for \'options.forceOutput\''
+      )
+      t.end()
+    })
+  }
+)
+
+test.cb(
+  'metalsmith-concat should override existing file check if forceOutput is enabled',
+  t => {
+    t.plan(2)
+    const files = {
+      'output/path1': { contents: Buffer.from('test123') },
+      'output/path2': { contents: Buffer.from('456test') },
+    }
+    const plugin = concat({
+      files: ['output/path1', 'output/path2'],
+      output: 'output/path',
+      forceOutput: true,
+    })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'output/path': { contents: Buffer.from('test123\n456test\n') },
+      })
+      t.end()
+    })
+  }
+)
+
+// https://github.com/aymericbeaumet/metalsmith-concat/issues/9
+test.cb(
+  'metalsmith-concat should not concatenate already concatenated files by default',
+  t => {
+    t.plan(2)
+    const files = {
+      'js/jquery.js': { contents: Buffer.from('jquery') },
+      'js/bootstrap/1.js': { contents: Buffer.from('bootstrap1') },
+      'js/bootstrap/2.js': { contents: Buffer.from('bootstrap2') },
+      'js/other/1.js': { contents: Buffer.from('other1') },
+      'js/other/2.js': { contents: Buffer.from('other2') },
+    }
+    const plugin = concat({
+      files: ['js/jquery.js', 'js/bootstrap/*.js', 'js/**/*.js'],
+      output: 'js/app.js',
+    })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'js/app.js': {
+          contents: Buffer.from(
+            'jquery\nbootstrap1\nbootstrap2\nother1\nother2\n'
+          ),
+        },
+      })
+      t.end()
+    })
+  }
+)
+
+// https://github.com/aymericbeaumet/metalsmith-concat/issues/8
+test.cb(
+  'metalsmith-concat should normalize options.output and options.files to be cross-platform compatible',
+  t => {
+    t.plan(2)
+    const files = {
+      'js/jquery.js': { contents: Buffer.from('jquery') },
+    }
+    const plugin = concat({ files: ['js\\jquery.js'], output: 'js\\app.js' })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'js/app.js': { contents: Buffer.from('jquery\n') },
+      })
+      t.end()
+    })
+  }
+)
+
+test.cb(
+  'metalsmith-concat should not include files from options.searchPaths by default',
+  t => {
+    t.plan(2)
+    const files = {}
+    const plugin = concat({ files: ['**/*.md'], output: 'output' })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.true(files.output.contents.equals(Buffer.from('')))
+      t.end()
+    })
+  }
+)
+
+test.cb(
+  'metalsmith-concat should resolve relative searchPaths from the project root (as a string)',
+  t => {
+    t.plan(2)
+    const files = {}
+    const plugin = concat({
+      files: ['**/*.md'],
+      output: 'output',
+      searchPaths: '.',
+    })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.true(
+        files.output.contents.equals(Buffer.from('anotherdir\n\nroot\n\n'))
+      )
+      t.end()
+    })
+  }
+)
+
+test.cb(
+  'metalsmith-concat should resolve relative searchPaths from the project root (as an array of strings)',
+  t => {
+    t.plan(2)
+    const files = {}
+    const plugin = concat({
+      files: ['**/*.md'],
+      output: 'output',
+      searchPaths: ['.'],
+    })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.true(
+        files.output.contents.equals(Buffer.from('anotherdir\n\nroot\n\n'))
+      )
+      t.end()
+    })
+  }
+)
+
+// serial because of mocking
+test.serial.cb('metalsmith-concat should forward glob errors', t => {
+  t.plan(2)
+  sinon.replace(require('glob'), 'glob', (_pattern, _options, callback) =>
+    callback(new Error('glob.glob error'))
+  )
+  const files = {}
+  const plugin = concat({
+    files: ['**/*.md'],
+    output: 'output',
+    searchPaths: ['.'],
+  })
+  plugin(files, metalsmithFixture(), error => {
+    t.true(error instanceof Error)
+    t.is(error.message, 'glob.glob error')
+    t.end()
+  })
+})
+
+// serial because of mocking
+test.serial.cb('metalsmith-concat should forward fs.readFile errors', t => {
+  t.plan(2)
+  sinon.replace(require('fs'), 'readFile', (_filepath, callback) =>
+    callback(new Error('fs.readFile error'))
+  )
+  const files = {}
+  const plugin = concat({
+    files: ['**/*.md'],
+    output: 'output',
+    searchPaths: ['.'],
+  })
+  plugin(files, metalsmithFixture(), error => {
+    t.true(error instanceof Error)
+    t.is(error.message, 'fs.readFile error')
+    t.end()
+  })
+})
+
+// https://github.com/aymericbeaumet/metalsmith-concat/issues/25#issue-145523237
+test.cb(
+  'metalsmith-concat should respect the order given by the metalsmith files object',
+  t => {
+    t.plan(2)
+    const files = {
+      '01_foo.css': { contents: Buffer.from('first') },
+      '02_bar.css': { contents: Buffer.from('second') },
+    }
+    const plugin = concat({ output: 'output/path' })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'output/path': { contents: Buffer.from('first\nsecond\n') },
+      })
+      t.end()
+    })
+  }
+)
+
+// https://github.com/aymericbeaumet/metalsmith-concat/issues/26
+test.cb(
+  'metalsmith-concat should simplify the given path in the pattern',
+  t => {
+    t.plan(2)
+    const files = {
+      'foo/bar/baz': { contents: Buffer.from('foobarbaz') },
+    }
+    const plugin = concat({
+      files: 'foo//\\\\bar\\\\//baz',
+      output: 'output/path',
+    })
+    plugin(files, metalsmithFixture(), error => {
+      t.falsy(error)
+      t.deepEqual(files, {
+        'output/path': { contents: Buffer.from('foobarbaz\n') },
+      })
+      t.end()
+    })
+  }
+)
