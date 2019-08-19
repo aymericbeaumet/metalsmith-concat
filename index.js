ef93e9390412e9919786d496857d06e91a7090aa
Aymeric Beaumet <hi@aymericbeaumet.com>
2019-08-20 01:24:28 +0200

    docs: add comment about shallow flattening

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function gathererFromSearchPaths(rootPath, searchPaths, pattern) {
           return done(error)
         }
 
-        return done(null, [].concat(...filesContents))
+        return done(null, [].concat(...filesContents)) // shallow flatten
       }
     )
   }
