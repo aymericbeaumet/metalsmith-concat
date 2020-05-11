9dae4ea74bd58e671e5e28f43f76462751633177
Aymeric Beaumet <hi@aymericbeaumet.com>
2020-05-11 19:43:09 +0200

    chore: bump dependencies

--- example/build.js
+++ example/build.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
-const metalsmith = require('metalsmith')
-const metalsmithConcat = require('..')
+const metalsmith = require('metalsmith');
+const metalsmithConcat = require('..');
 
 metalsmith(__dirname)
-  .use(
-    metalsmithConcat({
-      files: '**/*.css',
-      output: 'main.css',
-    })
-  )
-  .build(error => {
-    if (error) {
-      throw error
-    }
-  })
+	.use(
+		metalsmithConcat({
+			files: '**/*.css',
+			output: 'main.css'
+		})
+	)
+	.build(error => {
+		if (error) {
+			throw error;
+		}
+	});
--- index.js
+++ index.js
@@ -1,135 +1,134 @@
-const fs = require('fs')
-const path = require('path')
-const async = require('async')
-const glob = require('glob')
-const minimatch = require('minimatch')
+const fs = require('fs');
+const path = require('path');
+const async = require('async');
+const glob = require('glob');
+const minimatch = require('minimatch');
 
-function gathererFromSourceDirectory(source, pattern, { keepConcatenated }) {
-  return done => {
-    // We loop over all the files Metalsmith knows of and return an array of all
-    // the files contents matching the given pattern
-    return done(
-      null,
-      Object.keys(source).reduce((acc, filepath) => {
-        if (minimatch(filepath, pattern)) {
-          acc.push(source[filepath].contents)
-          if (!keepConcatenated) {
-            delete source[filepath]
-          }
-        }
+function gathererFromSourceDirectory(source, pattern, {keepConcatenated}) {
+	return done => {
+		// We loop over all the files Metalsmith knows of and return an array of all
+		// the files contents matching the given pattern
+		return done(
+			null,
+			Object.keys(source).reduce((acc, filepath) => {
+				if (minimatch(filepath, pattern)) {
+					acc.push(source[filepath].contents);
+					if (!keepConcatenated) {
+						delete source[filepath];
+					}
+				}
 
-        return acc
-      }, [])
-    )
-  }
+				return acc;
+			}, [])
+		);
+	};
 }
 
 function gathererFromSearchPaths(rootPath, searchPaths, pattern) {
-  return done => {
-    // We loop over the search paths and return an array of all the files
-    // contents matching the given pattern
-    async.map(
-      searchPaths,
-      (searchPath, callback) => {
-        const globPattern = path.resolve(rootPath, searchPath, pattern)
-        glob.glob(
-          globPattern,
-          {
-            ignore: path.resolve(rootPath, 'src/**/*'),
-            minimatch,
-            nodir: true,
-          },
-          (error, filepaths) => {
-            if (error) {
-              return callback(error)
-            }
+	return done => {
+		// We loop over the search paths and return an array of all the files
+		// contents matching the given pattern
+		async.map(
+			searchPaths,
+			(searchPath, callback) => {
+				const globPattern = path.resolve(rootPath, searchPath, pattern);
+				glob.glob(
+					globPattern,
+					{
+						ignore: path.resolve(rootPath, 'src/**/*'),
+						minimatch,
+						nodir: true
+					},
+					(error, filepaths) => {
+						if (error) {
+							return callback(error);
+						}
 
-            async.map(
-              filepaths.map(filepath => path.resolve(rootPath, filepath)),
-              fs.readFile,
-              callback
-            )
-          }
-        )
-      },
-      (error, filesContents) => {
-        if (error) {
-          return done(error)
-        }
+						async.map(
+							filepaths.map(filepath => path.resolve(rootPath, filepath)),
+							fs.readFile,
+							callback
+						);
+					}
+				);
+			},
+			(error, filesContents) => {
+				if (error) {
+					return done(error);
+				}
 
-        return done(null, [].concat(...filesContents)) // Shallow flatten
-      }
-    )
-  }
+				return done(null, [].concat(...filesContents)); // Shallow flatten
+			}
+		);
+	};
 }
 
 function metalsmithifyPath(path) {
-  return path.replace(/[/\\]+/g, '/')
+	return path.replace(/[/\\]+/g, '/');
 }
 
 module.exports = (options = {}) => {
-  if (!(typeof options.output === 'string' && options.output.length > 0)) {
-    throw new Error(
-      '`options.output` is mandatory and has to be a non-empty string'
-    )
-  }
+	if (!(typeof options.output === 'string' && options.output.length > 0)) {
+		throw new Error(
+			'`options.output` is mandatory and has to be a non-empty string'
+		);
+	}
 
-  const output = metalsmithifyPath(options.output)
+	const output = metalsmithifyPath(options.output);
 
-  const patterns = (Array.isArray(options.files)
-    ? options.files
-    : typeof options.files === 'string'
-    ? [options.files]
-    : ['**/*']
-  ).map(metalsmithifyPath)
+	const patterns = (Array.isArray(options.files) ?
+		options.files :
+		(typeof options.files === 'string' ?
+			[options.files] :
+			['**/*'])
+	).map(p => metalsmithifyPath(p));
 
-  const EOL =
-    typeof options.insertNewline === 'string'
-      ? options.insertNewline
-      : options.insertNewline === false
-      ? ''
-      : '\n'
+	const EOL = typeof options.insertNewline === 'string' ?
+		options.insertNewline :
+		(options.insertNewline === false ?
+			'' :
+			'\n');
 
-  const searchPaths = Array.isArray(options.searchPaths)
-    ? options.searchPaths
-    : typeof options.searchPaths === 'string'
-    ? [options.searchPaths]
-    : []
+	const searchPaths = Array.isArray(options.searchPaths) ?
+		options.searchPaths :
+		(typeof options.searchPaths === 'string' ?
+			[options.searchPaths] :
+			[]);
 
-  const { forceOutput = false, keepConcatenated = false } = options
+	const {forceOutput = false, keepConcatenated = false} = options;
 
-  return (files, metalsmith, done) => {
-    if (output in files && !forceOutput) {
-      return done(
-        new Error(
-          `The file "${output}" already exists, see the documentation for 'options.forceOutput'`
-        )
-      )
-    }
+	return (files, metalsmith, done) => {
+		if (output in files && !forceOutput) {
+			return done(
+				new Error(
+					`The file "${output}" already exists, see the documentation for 'options.forceOutput'`
+				)
+			);
+		}
 
-    // We reduce the array of patterns into an array of gatherers
-    const gatherers = patterns.reduce(
-      (acc, pattern) => [
-        ...acc,
-        gathererFromSourceDirectory(files, pattern, { keepConcatenated }),
-        gathererFromSearchPaths(metalsmith._directory, searchPaths, pattern),
-      ],
-      []
-    )
+		// We reduce the array of patterns into an array of gatherers
+		const gatherers = patterns.reduce(
+			(acc, pattern) => [
+				...acc,
+				gathererFromSourceDirectory(files, pattern, {keepConcatenated}),
+				gathererFromSearchPaths(metalsmith._directory, searchPaths, pattern)
+			],
+			[]
+		);
 
-    // Gather all the files contents and generate the final concatenated file
-    async.parallel(gatherers, (error, gatherersResults) => {
-      if (error) {
-        return done(error)
-      }
+		// Gather all the files contents and generate the final concatenated file
+		async.parallel(gatherers, (error, gatherersResults) => {
+			if (error) {
+				return done(error);
+			}
 
-      const filesContents = [
-        ...[].concat(...gatherersResults), // Shallow flatten the results from each gatherers [[a], [b]] -> [a, b]
-        '', // Append an empty string so that the final join result includes a trailing new line
-      ]
+			const filesContents = [
+				...[].concat(...gatherersResults), // Shallow flatten the results from each gatherers [[a], [b]] -> [a, b]
+				'' // Append an empty string so that the final join result includes a trailing new line
+			];
 
-      files[output] = { contents: Buffer.from(filesContents.join(EOL)) }
-      return done()
-    })
-  }
-}
+			files[output] = {contents: Buffer.from(filesContents.join(EOL))};
+			return done();
+		});
+	};
+};
